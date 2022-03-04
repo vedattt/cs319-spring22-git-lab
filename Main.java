b89abb00c03c37cd453857eed4b1ba2052820f8d
Ved <dev@vedat.xyz>
2022-03-04 10:39:06 +0300

    add evaluation code in main method

--- Main.java
+++ Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-
+        NewCalculator calculator = new NewCalculator();
+        
+        // Try division
+        calculator.Divide(100, 12);
+        System.out.println(calculator.getLastResult());
+        
+        // Try addition
+        calculator.Add(10, 35);
+        System.out.println(calculator.getLastResult());
+        
+        // Try subtraction
+        calculator.Subtract(50, 15);
+        System.out.println(calculator.getLastResult());
     }
 }
