b4d767e793b4478526a722378fcf848eb583d2f9
Ved <dev@vedat.xyz>
2022-03-04 10:40:48 +0300

    add evaluation code for Multiply

--- Main.java
+++ Main.java
@@ -13,5 +13,9 @@ public class Main {
         // Try subtraction
         calculator.Subtract(50, 15);
         System.out.println(calculator.getLastResult());
+        
+        // Try Multiplication
+        calculator.Multiply(10, 30);
+        System.out.println(calculator.getLastResult());
     }
 }
